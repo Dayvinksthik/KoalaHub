60e855998a95e09ef24e8ed8b1796b256a8bd9bf
Dayvinksthik <daylightgvguv@gmail.com>
2025-12-23 14:36:31 +0800

    Add files via upload

--- website/wsgi.py
+++ website/wsgi.py
@@ -10,5 +10,8 @@ from website.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    # Get port from environment variable, default to 10000
     port = int(os.environ.get("PORT", 10000))
+    
+    # IMPORTANT: Use 0.0.0.0 to bind to all interfaces
     app.run(host='0.0.0.0', port=port, debug=False)
